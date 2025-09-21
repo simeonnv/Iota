bf4d3dd8e2179f3e35539162ff02792247da35b4
Simeon <111970903+simeonnv@users.noreply.github.com>
2025-09-21 22:15:42 +0300

    use_state hook

--- src/bins/iota_desktop/src/main.rs
+++ src/bins/iota_desktop/src/main.rs
@@ -1,5 +1,8 @@
 use eframe::egui;
+use egui::Ui;
 use egui_transition_animation::prelude::*;
+use hooks::use_state;
+use hooks::use_state::use_state;
 
 struct MyApp {
     name: String,
@@ -26,27 +29,37 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ctx.style_mut(|style| style.animation_time = 0.4);
-            egui::CentralPanel::default().show(ctx, |ui| {
-                ui.horizontal(|ui| {
-                    ui.selectable_value(&mut self.page, Page::Page1, "Page 1");
-                    ui.selectable_value(&mut self.page, Page::Page2, "Page 2");
-                    ui.selectable_value(&mut self.page, Page::Page3, "Page 3");
-                });
 
-                animated_pager(
-                    ui,
-                    self.page.clone(),
-                    &TransitionStyle::fade(ui),
-                    egui::Id::new("pager"),
-                    |ui, page| match page {
-                        Page::Page1 => ui.label("Hello from page 1"),
-                        Page::Page2 => ui.heading("Hello from page 2"),
-                        Page::Page3 => ui.monospace("Hello from page 3"),
-                    },
-                )
+            ui.horizontal(|ui| {
+                ui.selectable_value(&mut self.page, Page::Page1, "Page 1");
+                ui.selectable_value(&mut self.page, Page::Page2, "Page 2");
+                ui.selectable_value(&mut self.page, Page::Page3, "Page 3");
             });
+
+            let num = use_state!(0_i32);
+
+            if ui.button("+").clicked() {
+                num.set(num.get().saturating_add(1));
+            }
+            if ui.button("-").clicked() {
+                num.set(num.get().saturating_add(-1));
+            }
+
+            ui.label(num.get().to_string());
+
+            animated_pager(
+                ui,
+                self.page.clone(),
+                &TransitionStyle::fade(ui),
+                egui::Id::new("pager"),
+                |ui, page| match page {
+                    Page::Page1 => ui.label("Hello from page 1"),
+                    Page::Page2 => ui.heading("Hello from page 2"),
+                    Page::Page3 => ui.monospace("Hello from page 3"),
+                },
+            )
         });
     }
 }
